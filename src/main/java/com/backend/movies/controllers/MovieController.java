cb9eafa444a82759a2fcacfd6615dfe77a61480c
luisaAran <luisamariaarangolopez@gmail.com>
2025-09-20 12:43:44 -0500

    Commit inicial

---
+++ src/main/java/com/backend/movies/controllers/MovieController.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.movies.controllers;
+
+import com.backend.movies.models.Movie;
+import com.backend.movies.repositories.MovieRepository;
+import java.util.List;
+import java.util.Optional;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+    @Autowired
+    private MovieRepository movieRepository;
+    @CrossOrigin
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+    @CrossOrigin
+    @PostMapping
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie){
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.status( HttpStatus.CREATED).body(savedMovie);
+    }
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id){
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+
+    
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id){
+        movieRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updateMovie){
+        updateMovie.setId(id);
+        Movie saveMovie = movieRepository.save(updateMovie);
+        return ResponseEntity.ok(saveMovie);
+    }
+    
+    
+    @CrossOrigin
+    @GetMapping("/vote/{id}/{rating}")
+    public ResponseEntity<Movie> voteMovie(@PathVariable Long id, @PathVariable double rating){
+
+        Optional<Movie> optional = movieRepository.findById(id);
+        Movie movie = optional.get();
+
+        double newRating = ((movie.getVotes() * movie.getRating())+rating) / (movie.getVotes() + 1);
+
+        movie.setVotes(movie.getVotes() + 1);
+        movie.setRating(newRating);
+
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(savedMovie);
+
+    }
+
+    
+}
---
+++ src/main/java/com/backend/movies/models/Movie.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.movies.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int year;
+    private int votes;
+    private double rating;
+    private String description;
+    
+    @Column(name = "image_url")
+    private String imageUrl;
+    
+    
+    
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the year
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the votes
+     */
+    public int getVotes() {
+        return votes;
+    }
+
+    /**
+     * @param votes the votes to set
+     */
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    /**
+     * @return the rating
+     */
+    public double getRating() {
+        return rating;
+    }
+
+    /**
+     * @param rating the rating to set
+     */
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    /**
+     * @return the imageUrl
+     */
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    /**
+     * @param imageUrl the imageUrl to set
+     */
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
