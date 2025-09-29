f167c91c5a4582fd3c4ec6c693b0366349d94ac7
Luisa Maria Arango Lopez <168994007+luisaAran@users.noreply.github.com>
2025-09-29 05:47:15 -0500

    Expiracion del token actualizada

--- src/main/java/com/backend/movies/services/JwtService.java
+++ src/main/java/com/backend/movies/services/JwtService.java
@@ -40,7 +40,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
